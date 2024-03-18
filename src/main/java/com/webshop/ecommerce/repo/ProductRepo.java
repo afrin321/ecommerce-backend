3e8e3ecd3ff82f29dc33dc753c7cc05ccc7f3129
AfrinH <afrinhaque@outlook.com>
2024-03-18 20:49:15 +0600

    pagination

--- src/main/java/com/webshop/ecommerce/repo/ProductRepo.java
+++ src/main/java/com/webshop/ecommerce/repo/ProductRepo.java
@@ -1,10 +1,11 @@
 package com.webshop.ecommerce.repo;
 
 import com.webshop.ecommerce.model.Product;
+import lombok.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
 
